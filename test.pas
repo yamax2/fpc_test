798949e128f18918c63cab0732b90f702f21ff5a
Maxim Tretyakov <max@tretyakov-ma.ru>
2018-03-03 21:38:53 +0500

    thread manager

--- test.pas
+++ test.pas
@@ -1,32 +1,190 @@
 program Test;
 {$MODE OBJFPC}{$H+}
+{$define debug}
 
 uses
 {$ifdef unix}
   cthreads,
   cmem,
 {$endif}
+{$ifdef debug}
+  HeapTrc,
+{$endif}
   Classes, SysUtils, Process;
 
 type
-  TMyThread = class(TThread)
+  TMyThread = class;
+
+  { TMyThreadManager }
+
+  TMyThreadManager = class(TThread)
   private
-    FFileName: String;
+    FEvent: pRTLEvent;
+    FList: TThreadList;
   protected
     procedure Execute; override;
+    function GetNextThread: TMyThread; virtual;
+    function GetMaxThreadCount: Integer; virtual;
+    procedure Process(AFinishedThread: TMyThread = nil);
   public
-    constructor Create(CreateSuspended: Boolean; AFileName: String);
-    property Terminated;
+    constructor Create;
+    destructor Destroy; override;
+    procedure Interrupt;
+    property MaxThreadCount: Integer read GetMaxThreadCount;
   end;
+
+  { TMyThread }
+
+  TMyThread = class(TThread)
+  private
+    FManager: TMyThreadManager;
+  public
+    constructor Create(AManager: TMyThreadManager);
+    destructor Destroy; override;
+  end;
+
+{ TMyThread }
+
+constructor TMyThread.Create(AManager: TMyThreadManager);
+begin
+  FManager:=AManager;
+  inherited Create(True);
+  FreeOnTerminate:=True;
+end;
+
+destructor TMyThread.Destroy;
+begin
+  FManager.Process(Self);
+  inherited;
+end;
+
+{ TMyThreadManager }
+
+constructor TMyThreadManager.Create;
+begin
+  FList:=TThreadList.Create;
+  FEvent:=RTLEventCreate;
+  inherited Create(False);
+  FreeOnTerminate:=True;
+end;
+
+destructor TMyThreadManager.Destroy;
+begin
+  FList.Free;
+  RTLeventdestroy(FEvent);
+  inherited;
+end;
+
+procedure TMyThreadManager.Interrupt;
+begin
+  RtlEventSetEvent(FEvent);
+end;
+
+function TMyThreadManager.GetNextThread: TMyThread;
+begin
+  Result:=nil;
+end;
+
+procedure TMyThreadManager.Process(AFinishedThread: TMyThread = nil);
+var
+  List: TList;
+  NextThread: TMyThread;
+begin
+ List:=FList.LockList;
+ try
+   if AFinishedThread <> nil then List.Remove(AFinishedThread);
+   if not (List.Count < GetMaxThreadCount) or Terminated then Exit;
+
+   repeat
+     NextThread:=GetNextThread;
+     if NextThread <> nil then
+     begin
+       List.Add(NextThread);
+       NextThread.Start;
+     end;
+   until (NextThread = nil) or not (List.Count < GetMaxThreadCount);
+
+   if NextThread = nil then Interrupt;
+ finally
+   FList.UnlockList;
+ end;
+end;
+
+function TMyThreadManager.GetMaxThreadCount: Integer;
+begin
+  Result:=2;
+end;
+
+procedure TMyThreadManager.Execute;
+var
+  List: TList;
+  Handles: array of TThreadID;
+  Index: Integer;
+begin
+ Process;
+ RtlEventWaitFor(FEvent);
+ Terminate;
+
+ List:=FList.LockList;
+ try
+   SetLength(Handles, List.Count);
+   for Index:=0 to List.Count - 1 do Handles[Index]:=TThread(List[Index]).Handle;
+ finally
+   FList.UnlockList;
+ end;
+
+ for Index:=0 to Length(Handles) - 1 do
+   WaitForThreadTerminate(Handles[Index], 0);
+end;
 
 const
   BUF_SIZE = 16384;
 
-procedure ReadIt(Thread: TMyThread);
+type
+
+  { TTestThread }
+
+  TTestThread = class(TMyThread)
+  private
+    FNumber: Integer;
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(AManager: TMyThreadManager; ANumber: Integer);
+  end;
+
+  { TTestThreadManager }
+
+  TTestThreadManager = class(TMyThreadManager)
+  private
+    FCount: Integer;
+  protected
+    function GetNextThread: TMyThread; override;
+  public
+    constructor Create;
+  end;
+
+{ TTestThreadManager }
+
+function TTestThreadManager.GetNextThread: TMyThread;
+begin
+  Inc(FCount);
+  if FCount <= 9 then Result:=TTestThread.Create(Self, FCount) else Result:=nil;
+end;
+
+constructor TTestThreadManager.Create;
+begin
+  FCount:=0;
+  inherited;
+end;
+
+{ TTestThread }
+
+procedure TTestThread.Execute;
 var
   AProcess: TProcess;
-  Buffer: array[1..BUF_SIZE] of byte;
-  BytesRead: longint;
+  Buffer: array[1..BUF_SIZE] of Byte;
+  BytesRead: Longint;
   OutputStream: TFileStream;
 begin
   AProcess:=TProcess.Create(nil);
@@ -41,13 +199,12 @@ begin
     end;
 
     AProcess.Execute;
-
-    OutputStream:=TFileStream.Create(Thread.FFileName, fmCreate);
+    OutputStream:=TFileStream.Create(Format('z%d.txt', [FNumber]), fmCreate);
     try
       repeat
         BytesRead:=AProcess.Output.Read(Buffer, BUF_SIZE);
         OutputStream.Write(Buffer, BytesRead);
-      until Thread.Terminated or (BytesRead = 0);
+      until Terminated or (BytesRead = 0);
     finally
       OutputStream.Free;
     end;
@@ -56,21 +213,15 @@ begin
   end;
 end;
 
-constructor TMyThread.Create(CreateSuspended : Boolean; AFileName: String);
+constructor TTestThread.Create(AManager: TMyThreadManager; ANumber: Integer);
 begin
-  FFileName:=AFileName;
-  inherited Create(CreateSuspended);
-  FreeOnTerminate:=True;
+ FNumber:=ANumber;
+ inherited Create(AManager);
 end;
 
-procedure TMyThread.Execute;
+var
+  T: TThread;
 begin
-  ReadIt(Self);
-end;
-
-var 
-  t: TMyThread;
-begin
-  t:=TMyThread.Create(False, '1.txt');
-  t.WaitFor;
+  T:=TTestThreadManager.Create;
+  T.WaitFor;
 end.
