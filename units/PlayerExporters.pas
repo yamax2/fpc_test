ff1b78849c06084fc86ee45c86c6525f612e4815
Maxim Tretyakov <max@tretyakov-ma.ru>
2018-06-10 00:02:01 +0500

    exporters (wip)

--- units/PlayerExporters.pas
+++ units/PlayerExporters.pas
@@ -22,6 +22,8 @@ type
     FOnFinish: TNotifyEvent;
     FOnProcess: TPlayerProcessEvent;
     FSessionID, FDir: String;
+  protected
+    procedure DoFinish; virtual;
   public
     constructor Create(ASessionID: String);
     destructor Destroy; override;
@@ -34,8 +36,34 @@ type
     property OnProcess: TPlayerProcessEvent read FOnProcess write FOnProcess;
   end;
 
-  TPlayerExporterManager = class(TPlayerThreadManager)
+  { TPlayerExporterManager }
 
+  TPlayerExporterManager = class(TPlayerThreadManager)
+  private
+    FExporter: TPlayerExporter;
+  protected
+    function GetNextThread: TPlayerThread; override;
+    procedure Execute; override;
+  public
+    constructor Create(AExporter: TPlayerExporter);
+    procedure Interrupt(const Force: Boolean = False); override;
+
+    property Exporter: TPlayerExporter read FExporter;
+  end;
+
+  { TPlayerExporterThread }
+
+  TPlayerExporterThread = class(TPlayerThread)
+  private
+    FTripID: Integer;
+    function GetManager: TPlayerThreadManager;
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(AManager: TPlayerThreadManager; const ATripID: Integer);
+
+    property Manager: TPlayerThreadManager read GetManager;
+    property TripID: Integer read FTripID;
   end;
 
 implementation
@@ -43,7 +71,89 @@ implementation
 uses
   dmxPlayer, PlayerLogger, PlayerOptions, FileUtil;
 
+{ TPlayerExporterThread }
+
+function TPlayerExporterThread.GetManager: TPlayerThreadManager;
+begin
+  Result:=inherited Manager as TPlayerThreadManager;
+end;
+
+procedure TPlayerExporterThread.Execute;
+begin
+  try
+    if not Terminated then
+      //Synchronize(@Exporter.DoProcess);
+  except
+    on E: Exception do
+    begin
+      //logger.Log('error on exporter thread %d, session %s, text: %s',
+      //  [FIndex, Extractor.FSessionID, E.Message]);
+      raise;
+    end;
+  end;
+end;
+
+constructor TPlayerExporterThread.Create(AManager: TPlayerThreadManager;
+  const ATripID: Integer);
+begin
+  inherited Create(AManager);
+  FTripID:=ATripID;
+end;
+
+{ TPlayerExporterManager }
+
+function TPlayerExporterManager.GetNextThread: TPlayerThread;
+begin
+  Result:=nil;
+end;
+
+procedure TPlayerExporterManager.Execute;
+begin
+  inherited;
+
+  if not (False) then
+  begin
+    Exporter.FExported:=True;
+  end;
+
+  Synchronize(@FExporter.DoFinish);
+end;
+
+constructor TPlayerExporterManager.Create(AExporter: TPlayerExporter);
+begin
+ inherited Create;
+ FExporter:=AExporter;
+end;
+
+procedure TPlayerExporterManager.Interrupt(const Force: Boolean);
+var
+  List: TList;
+  Index: Integer;
+
+  CurThread: TPlayerExporterThread;
+begin
+  inherited;
+
+  if not Force then Exit;
+  List:=ThreadList.LockList;
+  try
+    for Index:=0 to List.Count - 1 do
+    begin
+      CurThread:=TPlayerExporterThread(List[Index]);
+      CurThread.Terminate;
+    end;
+  finally
+    ThreadList.UnlockList;
+  end;
+end;
+
 { TPlayerExporter }
+
+procedure TPlayerExporter.DoFinish;
+begin
+  if @FOnFinish <> nil then
+    FOnFinish(Self);
+end;
 
 constructor TPlayerExporter.Create(ASessionID: String);
 begin
@@ -55,24 +165,26 @@ begin
   FDir:=IncludeTrailingPathDelimiter(FDir + 'html');
   FDir:=IncludeTrailingPathDelimiter(FDir + ASessionID);
 
+  DeleteDirectory(FDir, False);
+  logger.log('recreating dir: %s', [FDir]);
   ForceDirectories(FDir);
+
   if dmPlayer <> nil then ;
 end;
 
 destructor TPlayerExporter.Destroy;
 begin
-  logger.Log('export finished');
-  logger.log('deleting dir: %s', [FDir]);
-  DeleteDirectory(FDir, False);
+  logger.Log('export finished: %s', [FSessionID]);
 
   inherited;
 end;
 
 function TPlayerExporter.ExportData: TPlayerThreadManager;
 begin
-  Result:=nil;
-
   logger.Log('exporting session: %s', [FSessionID]);
+
+  Result:=TPlayerExporterManager.Create(Self);
+  Result.Start;
 end;
 
 end.
